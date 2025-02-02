79ff9e291fa53998215b0a61cea3ab7d3a9b5505
Xiao Jiang <bjjx1999@live.com>
2025-02-01 16:57:40 -0800

    Render node code cleanup

--- gamms/VisualizationEngine/Nodes/render_node.py
+++ gamms/VisualizationEngine/Nodes/render_node.py
@@ -1,51 +1,31 @@
-from gamms.VisualizationEngine import Color, Shape
+from gamms.VisualizationEngine import Shape
 from typing import Dict, Any
 
 
 class RenderNode:
     def __init__(self, data: Dict[str, Any]):
-        # self._x = x
-        # self._y = y
-        # self._layer = layer
-        # self._color = color
         self._data = data
 
     @property
-    def data(self):
+    def data(self) -> Dict[str, Any]:
         return self._data
 
     @property
-    def x(self):
+    def x(self) -> float | None:
         return self._data['x']
-    
-    # @x.setter
-    # def x(self, x):
-    #     self._x = x
 
     @property
-    def y(self):
+    def y(self) -> float | None:
         return self._data['y']
     
-    # @y.setter
-    # def y(self, y):
-    #     self._y = y
-
     @property
-    def layer(self):
-        return self._data['layer']
-    
-    @property
-    def color(self):
+    def color(self) -> tuple | None:
         return self._data['color']
     
     @property
-    def shape(self) -> Shape:
+    def shape(self) -> Shape | None:
         return self._data['shape']
     
     @property
-    def drawer(self):
+    def drawer(self) -> callable | None:
         return self._data['drawer']
-    
-    # @color.setter
-    # def color(self, color):
-    #     self._color = color
