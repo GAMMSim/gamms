f7d1836931f30b03120a0da993b45b4ce7e93e7a
Jai Malegaonkar <jai.malegaonkar@gmail.com>
2025-03-04 20:37:42 -0800

    created .osm files and tested

--- examples/base/create_graph.py
+++ examples/base/create_graph.py
@@ -3,7 +3,8 @@ import gamms
 import pickle
 # Create a graph
 
-G = gamms.osm.create_osm_graph(location, resolution=resolution)
+# G = gamms.osm.create_osm_graph(location, resolution=resolution)
+G = gamms.osm.graph_from_xml(filepath="/Users/jmalegaonkar/Desktop/gamms/examples/dummies/circle.osm", resolution=resolution)
 
 # Save the graph
 with open(graph_path, 'wb') as f:
