ad0cd6da362a3154abd78c8ecf87a0b1a2c9d4b2
bridgesign <eyeoeternity@gmail.com>
2025-03-07 05:14:04 +0000

    Restore create graph

--- examples/base/create_graph.py
+++ examples/base/create_graph.py
@@ -3,9 +3,7 @@ import gamms
 import pickle
 # Create a graph
 
-# G = gamms.osm.create_osm_graph(location, resolution=resolution)
-# print(G)
-G = gamms.osm.graph_from_xml(filepath="/Users/jmalegaonkar/Desktop/gamms/examples/dummies/grid_small.osm", resolution=1)
+G = gamms.osm.create_osm_graph(location, resolution=resolution)
 print(G)
 # Save the graph
 with open(graph_path, 'wb') as f:
